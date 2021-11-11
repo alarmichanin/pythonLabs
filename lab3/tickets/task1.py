caa11a1b00059e3ddf798030c746cba29ecd29ba
ALARMich <55944078+alarmichanin@users.noreply.github.com>
2021-11-11 23:56:00 +0200

    fixed some checks

--- lab3/tickets/task1.py
+++ lab3/tickets/task1.py
@@ -4,6 +4,7 @@ import datetime
 
 
 class ITEvent:
+    """class for making some It event """
     def __init__(self, price, day, month, year, num_of_tickets):
         if isinstance(price, (float, int)):
             self._price = price
@@ -16,6 +17,7 @@ class ITEvent:
             raise TypeError("Number of tickets have to be integer type only!")
 
     def write_to_JSON(self):
+        """method that write data to JSON file"""
         event_dict = {"id": str(uuid.uuid4()), "price": self._price, "number": self._num_of_tickets,
                       "date": str(self._date)}
         with open("ITevent.json", "w") as write_file:
@@ -23,6 +25,7 @@ class ITEvent:
 
 
 class RegularTicket:
+    """Base ticket class"""
     def __init__(self):
         self._id = uuid.uuid4()
         with open("ITevent.json") as file:
@@ -50,6 +53,7 @@ class RegularTicket:
 
 
 class AdvanceTicket(RegularTicket):
+    """Advance ticket class, that have 40% discount on the basic ticket price"""
     discount = 40
 
     def __init__(self):
@@ -59,6 +63,7 @@ class AdvanceTicket(RegularTicket):
 
 
 class LateTicket(RegularTicket):
+    """Late ticket class, that have 10% discount on the basic ticket price"""
     discount = 10
 
     def __init__(self):
@@ -68,6 +73,7 @@ class LateTicket(RegularTicket):
 
 
 class StudentTicket(RegularTicket):
+    """Student ticket class, that have 50% discount on the basic ticket price"""
     discount = 50
 
     def __init__(self):
@@ -77,7 +83,14 @@ class StudentTicket(RegularTicket):
 
 
 class Client:
+    """Client class that describe customer that order the ticket """
     def __init__(self, name, surname, is_student):
+        if not isinstance(name, str):
+            raise TypeError("Name have to be string type only!")
+        if not isinstance(surname, str):
+            raise TypeError("Surname have to be string type only!")
+        if not isinstance(is_student, bool):
+            raise TypeError("is_student have to be boolean type only")
         self.__name = name
         self.__surname = surname
         self.__is_student = is_student
@@ -117,6 +130,7 @@ class Client:
 
 
 class Order:
+    """Order class consist of the methods that allow to get full information about the ticket"""
     def __init__(self, client):
         if isinstance(client, Client):
             self.__client = client
@@ -125,17 +139,19 @@ class Order:
 
     @staticmethod
     def days_to_event():
+        """method to count difference in days between date of event and purchase date"""
         today = datetime.date.today()
         with open("ITevent.json") as file:
             event = json.load(file)
             date_of_event = datetime.datetime.strptime(event["date"], "%Y-%m-%d").date()
-        diff = (today - date_of_event).days
+        diff = (date_of_event - today).days
         return diff
 
     def choose_ticket(self, days):
+        """method to get right type of ticket"""
         if self.__client.is_student:
             return StudentTicket()
-        if isinstance(days, int):
+        if isinstance(days, int) and days > 0:
             if days >= 60:
                 return AdvanceTicket()
             elif days in range(11):
@@ -143,20 +159,23 @@ class Order:
             else:
                 return RegularTicket()
         else:
-            raise TypeError("Difference in date have to be integer type only!")
+            raise TypeError("Difference in date have to be integer type only and > 0 (check your dates)!")
 
     @staticmethod
     def get_ticket_price(ticket_type):
+        """method to get price of ticket"""
         if isinstance(ticket_type, (RegularTicket, StudentTicket, AdvanceTicket, LateTicket)):
             return ticket_type._price
         else:
             raise TypeError("ticket_type have to be one of the type of ticket")
 
     def ticket_maker(self, ticket_type, days_to_event):
+        """method to make dictionary with full information about ticket"""
         ticket_dict = {str(ticket_type._id): {}}
         ticket_dict[str(ticket_type._id)]["name"] = self.__client.name
         ticket_dict[str(ticket_type._id)]["surname"] = self.__client.surname
         ticket_dict[str(ticket_type._id)]["is_student"] = self.__client.is_student
+        ticket_dict[str(ticket_type._id)]["type_of_ticket"] = ticket_type._type
         ticket_dict[str(ticket_type._id)]["price"] = ticket_type._price
         ticket_dict[str(ticket_type._id)]["purchase_date"] = str(datetime.date.today())
         with open("ITevent.json") as file:
@@ -167,6 +186,8 @@ class Order:
         return ticket_dict
 
     def buy(self):
+        """method to buy a ticket, allows to make some note in json file about order, and control
+        if there are enough number of tickets """
         days = self.days_to_event()
         ticket_type = self.choose_ticket(days)
         with open("ITevent.json") as file:
@@ -194,9 +215,9 @@ class Order:
 
 
 def main():
-    google_ev = ITEvent(500, 31, 12, 2020, 15)
+    google_ev = ITEvent(500, 18, 11, 2021, 15)
     google_ev.write_to_JSON()
-    client = Client("Roman", "Tkachenko", True)
+    client = Client("Roman", "Tkachenko", False)
     ord = Order(client)
     print(ord)
     ord.buy()
